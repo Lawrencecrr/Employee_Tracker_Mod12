ae0b6d901df58cd91c28c086a0912118f93a20e9
Lawrencecrr <lawrenceschoolmail@gmail.com>
2022-09-10 17:11:47 -0400

    updated index.js

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql');
 const db = require('./db/connection');
+
+db.connect(err => {
+    if (err) throw err;
+    console.log('Database connected.');
+    chooseRequest();
+});
+
+const chooseRequest = () => {
+    inquirer.prompt({
+        type: 'list',
+        name: 'request',
+        message: 'what would you like to do?',
+        choices: []
+    })
+}
